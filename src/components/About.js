86255127590fcf5154e67dd7e3860171060b17df
shubhamthind <shubhamthind90@gmail.com>
2020-07-19 17:43:02 +0530

    Added header and about section

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const About = () =>( 
+
+    <div className="container">
+        <div className="about">
+            <h1>About Us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis consequuntur eum perspiciatis culpa dolor fuga nostrum, incidunt autem ea earum omnis quaerat sint ullam quas possimus in molestiae delectus velit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur corrupti esse doloribus blanditiis ipsa beatae tenetur cum illum quo, rem officiis! Fuga in facere assumenda, hic ad maiores modi nostrum. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla culpa id, adipisci cum vel, beatae dicta sunt placeat impedit maxime quod officia, sit reprehenderit eos. Ab aut inventore cupiditate reprehenderit! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque iste ducimus nesciunt minus harum, dignissimos sint dolor dicta est corporis? Iusto illum mollitia accusamus saepe dignissimos voluptas. Quae, consequuntur doloremque?</p>
+            <button className="btn btn-primary">View More</button>
+        </div>
+    </div>
+)
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Header = ()=>(
+
+    <div className="container">
+        <div className="header">
+        <h2 className="header-welcome-message">Welcome to</h2>
+        <h1 className="header-title">Your Friend</h1>
+        </div>
+    </div>
+
+)
